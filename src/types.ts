1f21500296f3a6d4c517f3172c9f729cab4e5fdb
Matias Wallenius <MatiWall@live.dk>
2026-07-05 13:19:55 +0200

    .

--- src/types.ts
+++ src/types.ts
@@ -1,19 +1,25 @@
-import { ZodObject, infer as zodInfer, } from "zod";
+import { ZodObject, ZodType, infer as zodInfer, } from "zod";
 import { ExtensionInputNode } from "./extension/ExtensionInputNode";
 import { ExtensionDataRef, ExtensionDataValue } from "./extension/ExtensionDataRef";
 
 
 export type NodeSpec = {
     input: Record<string, ExtensionInputNode>;
-    config: ZodObject<any>;
+    config: ZodType<any>;
     params: any;
 };
 
-export type ProviderInput<TInput> = TInput extends Record<string, ExtensionInputNode> ? {
-    [K in keyof TInput]: TInput[K] extends { allowMultiple: true }
-        ? any[]
-        : any;
- } : never;
+export type ProviderInput<TSpec extends NodeSpec> =
+    TSpec extends { input: infer I }
+        ? I extends Record<string, ExtensionInputNode>
+            ? {
+                  [K in keyof I]:
+                      I[K] extends { allowMultiple: true }
+                          ? any[]
+                          : any;
+              }
+            : {}
+        : {};
 
 
  export type Provider<TSpec extends NodeSpec> = (args: {
